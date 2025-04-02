5bd0dd126e9ef5641b56c133db2892de0b39e286
JesusRodrigoV <jaicelvelasco105@gmail.com>
2025-04-02 17:16:07 -0400

    Eliminado SSR

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,18 +6,13 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideHttpClient(withFetch()),
-    provideClientHydration(withEventReplay()),
     provideRouter(routes, withComponentInputBinding()),
-    provideAnimationsAsync(), provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
   ],
 };
